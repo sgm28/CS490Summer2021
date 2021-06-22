b26c92a37b078e9e80a40dedd136d5212b37583c
sgm28 <sgm28@njit.edu>
2021-06-22 17:23:26 -0400

    Create Displayuser.php
    Add code to display users on the admin page

---
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/Displayuser.php
@@ -0,0 +1,18 @@
+<?php
+
+		require '../../../dbh.inc.php';
+
+$sql = "SELECT usersName FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["usersName"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+
+?>
