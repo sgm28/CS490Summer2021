c7208f0197a2da5c1b6d3a5c8c00722bc35153e7
sgm28 <sgm28@njit.edu>
2021-06-22 18:25:41 -0400

    Update Home.php
    Uncommted the line because the bug in the last commite has been fix. I am to click on tabs again

--- WEBSITE WITH API/copy/FrontEnd/AdminPage/Home.php
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/Home.php
@@ -117,7 +117,7 @@ body {font-family: Arial;}
   <label for="vehicle3"> User 3</label>
   <hr>
   <button type="submit" class="deactivatebtn">Deactive</button>
-  <!-- <?php include './BackEnd/Displayuser.php'; ?> -->
+  <?php include './BackEnd/Displayuser.php'; ?>
 
 
 </form>
