9c222a45762e2c70100982e89cf007b7f9a9acc1
sgm28 <sgm28@njit.edu>
2021-06-22 18:02:18 -0400

    Update Home.php
    Testing there is a script bug

--- WEBSITE WITH API/copy/FrontEnd/AdminPage/Home.php
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/Home.php
@@ -122,9 +122,38 @@ body {font-family: Arial;}
 </form>
 </div>
 
-
+<script>
+function operation(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
    
 </body>
+<script>
+function operation(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
 </html> 
 <script>
 function operation(evt, cityName) {
