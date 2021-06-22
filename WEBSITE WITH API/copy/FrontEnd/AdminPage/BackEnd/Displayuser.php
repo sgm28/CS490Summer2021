8364acb7c5d94cc8c82f8451563e902a7966d5b6
sgm28 <sgm28@njit.edu>
2021-06-22 19:36:21 -0400

    Update Displayuser.php
    Testing to see if I can display users in an input tag

--- WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/Displayuser.php
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/Displayuser.php
@@ -8,11 +8,16 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["usersName"] . "<br>";
+    //echo "id: " . $row["usersName"] . "<br>";
+    echo "<input type='checkbox'" . "id='" .  $row["usersName"] . "'" . "name='" . $row["usersName"]
+	    . "'". "value='" . $row["usersName"] . "'";
   }
 } else {
   echo "0 results";
 }
 
 
+
+	  
 ?>
+ 
