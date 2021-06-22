16209e3d48a9cb76961e9d60648bcb9f37c6e784
sgm28 <sgm28@njit.edu>
2021-06-22 18:14:22 -0400

    Update Home.php

--- WEBSITE WITH API/copy/FrontEnd/AdminPage/Home.php
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/Home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript" src="./script.js"></script>
 <style>
 body {font-family: Arial;}
 
