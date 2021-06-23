80edb965262e001f5999a92d295ac1c782b230b6
sgm28 <sgm28@njit.edu>
2021-06-22 20:35:46 -0400

    Update Displayuser.php
    setting the correct attributes

--- WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/Displayuser.php
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/Displayuser.php
@@ -9,15 +9,15 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     //echo "id: " . $row["usersName"] . "<br>";
-    echo "<input type='checkbox'" . "id='" .  $row["usersName"] . "'" . "name='" . $row["usersName"]
-	    . "'". "value='" . $row["usersName"] . "'" . ">";
+    echo "<input type='checkbox'" . "id='" .  $row["usersName"] . "'" . "name='" . "id"
+	    . "'". "value='" . $row["usersId"] . "'" . ">";
 	 echo "<label for='" . $row["usersName"] . "'>" . $row["usersName"] . "</label><br>";
   }
 } else {
   echo "0 results";
 }
 
-
+ <input type="checkbox" id="user3" name="user3" value="user3">
 
 	  
 ?>
