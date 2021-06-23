64887a6fdbc4e0ee0310d5f734de16ebb3ee530c
sgm28 <sgm28@njit.edu>
2021-06-23 12:04:10 -0400

    Update DisplayuserForDelete.php
    Bug fix change usersId to usersName

--- WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/DisplayuserForDelete.php
+++ WEBSITE WITH API/copy/FrontEnd/AdminPage/BackEnd/DisplayuserForDelete.php
@@ -11,13 +11,13 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    //echo "id: " . $row["usersName"] . "<br>";
+
 
 	echo "<input type='radio'" . "id='" . $row["usersName"] . "'" .  "name='" . "id[]" . "'" . "value='"
 	. $row['usersId'] . "'" . ">";
-	echo "<label for='" . $row["usersId"] . "'>" . $row["usersName"] . "</label><br>";
+	echo "<label for='" . $row["usersName"] . "'>" . $row["usersName"] . "</label><br>";
 	
-    
+ 
 	
   }
 } else {
