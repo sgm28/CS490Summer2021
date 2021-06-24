3d1cc63bd96c6dba9e963d670ad7bdbafe992f83
sgm28 <sgm28@njit.edu>
2021-06-24 17:18:08 -0400

    Update recipe_search.php
    Added jquery

--- recipe_search.php
+++ recipe_search.php
@@ -12,7 +12,7 @@
   <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <style type="text/css">
