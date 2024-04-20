97aa02450a81a879d43d51ff9a88123edf4163c1
sgm28 <sakar.michel@gmail.com>
2024-04-19 21:31:19 -0400

    fix: Change database
    Change database name.

--- dbh.inc.php
+++ dbh.inc.php
@@ -9,7 +9,7 @@ $dBName = "sql5687781";
 $serverName = "sql2.njit.edu";
 $dBUsername = "sgm28";
 $dBPassword = "hM77K+B#qx(G+/v3";
-$dBName = "sgm28.UsersCS490";
+$dBName = "sgm28";
 
 
 /*
@@ -22,7 +22,15 @@ $dBName = "phptest";
 //Opens connection to the database
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
+
 //Open connection fails
 if(!$conn){
 	die("Connection Failed: " . mysqli_connect_error());
 }
+
+
+
+
+
+
+?>
